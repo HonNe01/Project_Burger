13086acb81e8c1f2c6d0319b6a858dcf5b6bcacf
kcsh0616-ai <kcsh0616@gmail.com>
2025-10-29 14:38:28 +0900

    fail
    fail sound +

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -23,7 +23,7 @@ public class SoundManager : MonoBehaviour
         Select, Success, Patty, Patty_Burn,
         Order, Good, Angry,
         Order_Kid, Good_Kid, Angry_Kid,
-        Order_Girl, Good_Girl, Angry_Girl, Click, Bell
+        Order_Girl, Good_Girl, Angry_Girl, Click, Bell, fail
     }
 
     void Awake()
